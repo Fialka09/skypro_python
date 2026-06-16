8ec670603ea70639a520baa0db69c82f1fc8bf2d
Elena <lenka17.09.03@gmail.com>
2026-06-16 22:46:41 +0300

    Задание 2: класс Smartphone и каталог

---
+++ 03_lesson/smartphone.py
@@ -0,0 +1,6 @@
+class Smartphone:
+
+    def __init__(self, phone_brand, phone_model, mobile_number):
+        self.phone_brand = phone_brand
+        self.phone_model = phone_model
+        self.mobile_number = mobile_number
