ca14e63df1bcff0e49c5d4317d610ba5cb32e42f
Elena <lenka17.09.03@gmail.com>
2026-07-22 19:47:13 +0300

    Добавила тест магазина с Page Object

---
+++ 07_lesson/test_shop.py
@@ -0,0 +1,33 @@
+import pytest
+
+from checkout_page import CheckoutPage
+from inventory_page import InventoryPage
+from login_page import LoginPage
+from cart_page import CartPage
+
+
+@pytest.mark.firefox
+def test_shop(firefox_driver):
+    login_page = LoginPage(firefox_driver)
+    login_page.open()
+    login_page.login()
+
+    inventory_page = InventoryPage(firefox_driver)
+    inventory_page.add_all_items()
+
+    assert (
+        inventory_page.get_cart_count() == "3"
+    ), "В корзине должно быть 3 товара!"
+
+    inventory_page.go_to_cart()
+
+    cart_page = CartPage(firefox_driver)
+    cart_page.checkout()
+
+    checkout_page = CheckoutPage(firefox_driver)
+    checkout_page.fill_form()
+    checkout_page.continue_order()
+    checkout_page.get_total()
+
+    total = checkout_page.get_total()
+    assert "Total: $58.29" in total, f"Неверная сумма: {total}"
